ff05fcd21a3af34d3c47934d5b06d779a949f85c
Martin Lehmann <martin.lehmann@accso.de>
2019-02-07 21:29:45 +0100

    Periodically save movies to S3 (every 5min), incl. all events and their prio and status

--- src/main/kotlin/de/mrtnlhmnn/berlinapple/application/PersistenceScheduler.kt
+++ src/main/kotlin/de/mrtnlhmnn/berlinapple/application/PersistenceScheduler.kt
@@ -1,48 +1,57 @@
 package de.mrtnlhmnn.berlinapple.application
 
+import com.amazonaws.services.s3.model.ListObjectsV2Request
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.amazonaws.util.StringUtils
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Types
 import de.mrtnlhmnn.berlinapple.data.*
+import de.mrtnlhmnn.berlinapple.infrastructure.S3Config
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import sun.awt.CharsetString
+import java.io.ByteArrayInputStream
+import java.io.File
 import java.lang.reflect.Type
 import java.net.URL
+import java.time.Instant
 import java.time.ZonedDateTime
 
 @Component
 @EnableScheduling
-class PersistenceScheduler(val movieRepo: MovieRepo) {
+class PersistenceScheduler(val movieRepo: MovieRepo, val s3Config: S3Config) {
     private val LOGGER = LoggerFactory.getLogger(this.javaClass)
 
+    val keyPrefix: String = "berlinapple2019/movies/movies-"
+
     @Scheduled(cron = "\${persistenceSchedule:0 * * * * *}")
-    fun saveMovies() {
-        val event = Event( ID("11"), ZonedDateTime.now(), ZonedDateTime.now(), "here", EventStatus.BOOKED)
-        val eventAsJson = event.toJSON()
-        val parsedEvent = eventAsJson.fromJSON<Event>()
-        println(event)
-        println(eventAsJson)
-        println(parsedEvent)
-        println(parsedEvent == event)
-
-        val movie = Movie(ID("12"), "title", "description", Prio.TOP, URL("http://localhost:8080"), mutableListOf(event))
-        val movieAsJson = movie.toJSON()
-        val parsedMovie = movieAsJson.fromJSON<Movie>()
-        println(movie)
-        println(movieAsJson)
-        println(parsedMovie)
-        println(parsedMovie == movie)
-
+    fun saveMoviesToS3() {
         val movieRepoAsJson = movieRepo.toJSON()
-        println(movieRepoAsJson)
+        val bytes = movieRepoAsJson.toByteArray(StringUtils.UTF8)
+        val bis = ByteArrayInputStream(bytes)
 
-        val movieList = movieRepoAsJson.listFromJSON<Movie>()
-        movieList!!.forEach {
-            println(it.description)
-            it.events.forEach {
-                println(" --> " + it)
-            }
+        val now = Instant.now().toEpochMilli().toString()
+        val key = keyPrefix + now + ".txt"
+
+        val metadata = ObjectMetadata()
+        metadata.contentLength = bytes.size.toLong()
+
+        try {
+            val putRequest = PutObjectRequest(s3Config.s3BucketName, key, ByteArrayInputStream(bytes), metadata)
+            s3Config.s3Client().putObject(putRequest)
         }
+        finally {
+            bis.close();
+        }
+
+        LOGGER.info("Writing {} movies (with {} events) as JSON to S3 with key {}, has {} bytes length",
+                movieRepo.getNumberOfMovies(),
+                movieRepo.getNumberOfEvents(),
+                key, metadata.contentLength)
     }
 }
---
+++ src/main/kotlin/de/mrtnlhmnn/berlinapple/infrastructure/S3Config.kt
@@ -0,0 +1,45 @@
+package de.mrtnlhmnn.berlinapple.infrastructure
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.util.StringUtils
+
+@Configuration
+open class S3Config {
+
+    @Value("\${aws.s3.access_key:}")
+    private val awsAccessKey: String? = null
+
+    @Value("\${aws.s3.secret_key:}")
+    private val awsSecretKey: String? = null
+
+    @Value("\${aws.s3.region:eu-central-1}")
+    private val s3Region: String? = null
+
+    @Value("\${aws.s3.bucketname}")
+    val s3BucketName: String? = null
+
+    @Bean
+    open fun s3Client(): AmazonS3 {
+        val amazonS3: AmazonS3
+
+        amazonS3 = if (StringUtils.isEmpty(awsAccessKey) || StringUtils.isEmpty(awsSecretKey)) {
+            AmazonS3ClientBuilder.standard()
+                    .withRegion(Regions.fromName(s3Region))
+                    .build()
+        } else {
+            AmazonS3ClientBuilder.standard()
+                    .withRegion(Regions.fromName(s3Region))
+                    .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(awsAccessKey, awsSecretKey)))
+                    .build()
+        }
+
+        return amazonS3
+    }
+}
