a5b269cd97933d92d70d5ef7f153159c9612c2db
Martin Lehmann <martin.lehmann@accso.de>
2019-12-28 17:15:26 +0100

    Renamed to test directory

--- src/test/java/de/mrtnlhmnn/berlinapple/BcryptPassword.java
+++ src/test/java/de/mrtnlhmnn/berlinapple/BcryptPassword.java
@@ -3,9 +3,10 @@ package de.mrtnlhmnn.berlinapple;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class BcryptPassword {
+    private static String password = "TODO";
+
     public static void main(String[] args) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String password = "TODO";
         for (int i=0; i<10; i++) {
             System.out.println(passwordEncoder.encode(password));
         }
