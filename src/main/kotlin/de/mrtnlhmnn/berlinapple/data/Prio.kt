63008c2ee0c284853cffd162a63d0deb16d0855a
Martin Lehmann <martin.lehmann@accso.de>
2019-12-30 13:27:40 +0100

    Cleanup

--- src/main/kotlin/de/mrtnlhmnn/berlinapple/data/Prio.kt
+++ src/main/kotlin/de/mrtnlhmnn/berlinapple/data/Prio.kt
@@ -11,7 +11,7 @@ data class Prio(val value: Int, val optional: String = ""): Comparable<Prio>, JS
     override fun toString() = value.toString()
 
     companion object {
-//TODO Wann braucht man const val, wann nur val?
+//TODO check when one needs const val, when only val?
         val TOP    = Prio(99)
         val NORMAL = Prio(50)
         val LOW    = Prio(1)
