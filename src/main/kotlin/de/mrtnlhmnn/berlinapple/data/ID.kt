f1dcada9b2b3be53126622854089d2894dd7f4ed
Martin Lehmann <martin.lehmann@accso.de>
2020-01-02 17:24:07 +0100

    Fix ID length to 8 characters

--- src/main/kotlin/de/mrtnlhmnn/berlinapple/data/ID.kt
+++ src/main/kotlin/de/mrtnlhmnn/berlinapple/data/ID.kt
@@ -1,16 +1,14 @@
 package de.mrtnlhmnn.berlinapple.data
 
-data class ID(var id: String) {
+data class ID(val id: String) {
 
     companion object {
-        private val SEPARATOR = "-"
-
+        private const val SEPARATOR = "-"
         fun createEventID(s: String) = ID("E" + SEPARATOR + s.hashAndHex())
-
         fun createMovieID(s: String) = ID("M" + SEPARATOR + s.hashAndHex())
     }
 
     override fun toString(): String = "$id"
 }
 
-private inline fun String.hashAndHex() = String.format("%016X", Math.abs(hashCode()))
+private inline fun String.hashAndHex() = String.format("%08X", Math.abs(hashCode()))
