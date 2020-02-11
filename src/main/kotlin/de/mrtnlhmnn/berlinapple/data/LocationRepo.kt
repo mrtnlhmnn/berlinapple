8d1775cd78b4ed6615b88c53792ac822090401e1
Martin Lehmann <martin.lehmann@accso.de>
2020-02-11 23:57:45 +0100

    Print out warning if location cannot be found in location repo

--- src/main/kotlin/de/mrtnlhmnn/berlinapple/data/LocationRepo.kt
+++ src/main/kotlin/de/mrtnlhmnn/berlinapple/data/LocationRepo.kt
@@ -1,21 +1,25 @@
 package de.mrtnlhmnn.berlinapple.data
 
 import de.mrtnlhmnn.berlinapple.infrastructure.JSONConvertable
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.util.regex.Pattern
 
 @Component
 class LocationRepo: HashMap<String, Location>(), JSONConvertable {
+
+    private val LOGGER = LoggerFactory.getLogger(this.javaClass)
+
     fun findLocationByString(locationStringFromProgram: String?): Location? {
         if (locationStringFromProgram == null) return null
 
         for (location in this.values.toList()) {
             // exact match?
             if (   (locationStringFromProgram.equals(location.name, ignoreCase = true))
-                    || (locationStringFromProgram.indexOf(location.name, ignoreCase = true) != -1)
-                    || (locationStringFromProgram.indexOf(getFirstWord(location.name), ignoreCase = true) != -1)
-                    || (location.name.indexOf(locationStringFromProgram, ignoreCase = true) != -1)
-                    || (location.name.indexOf(getFirstWord(locationStringFromProgram), ignoreCase = true) != -1)
+                || (locationStringFromProgram.indexOf(location.name, ignoreCase = true) != -1)
+                || (locationStringFromProgram.indexOf(getFirstWord(location.name), ignoreCase = true) != -1)
+                || (location.name.indexOf(locationStringFromProgram, ignoreCase = true) != -1)
+                || (location.name.indexOf(getFirstWord(locationStringFromProgram), ignoreCase = true) != -1)
             )
             {
                 return Location(locationStringFromProgram, location.address, location.url)
@@ -23,6 +27,9 @@ class LocationRepo: HashMap<String, Location>(), JSONConvertable {
         }
 
         // fallback if not found
+        LOGGER.warn("Could not find ${locationStringFromProgram} in the location repo, hence using as fallback. " +
+                "Please check location.txt, might want to add ${locationStringFromProgram} ...")
+
         return Location(name = locationStringFromProgram, address = null, url = null)
     }
 
