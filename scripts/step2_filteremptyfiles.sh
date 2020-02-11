bd15bfa78400bbf610f4f96d2458c706e28781bd
Martin Lehmann <martin.lehmann@accso.de>
2020-02-12 00:09:59 +0100

    Fixed wrong output

--- scripts/step2_filteremptyfiles.sh
+++ scripts/step2_filteremptyfiles.sh
@@ -8,7 +8,6 @@ pushd tmp > /dev/null
 for f in `find . -name "*.ics"`; do 
   g=`grep -H DTSTART:19700101T000000Z $f`
   if [ "$g" == "" ]; then
-     # echo $f is empty
      mv $f ../nonempty
   else
      mv $f ../empty
